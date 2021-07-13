1d3c8bcf585710a6656d455bce8b313728eb578f
vijayaraggavanp-ikomet <79459467+vijayaraggavanp-ikomet@users.noreply.github.com>
2021-07-14 00:49:01 +0530

    New banner,pages created
    New banner,pages created

---
+++ Controllers/EntryFormController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UmbracoInternal.Models;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoInternal.Controllers
+{
+    public class EntryFormController : SurfaceController
+    {
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/EntryForm/";
+        public ActionResult RenderForm()
+        {
+            EntryFormModel model = new EntryFormModel();
+            model.userName = "Vijay";
+            model.EmailID = "vijay@gmail.com";
+            model.PolicyNo = "33343";
+            model.DOB = "3rdOct2020";
+            ViewData["Data"] = model;
+            return PartialView(PARTIAL_VIEW_FOLDER + "RenderForm.cshtml");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(EntryFormModel model)
+        {
+            if (ModelState.IsValid)            {
+                
+                TempData["ContactSuccess"] = true;
+                return RedirectToCurrentUmbracoPage();
+            }
+            return CurrentUmbracoPage();
+        }
+    }
+}
---
+++ Models/EntryFormModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UmbracoInternal.Models
+{
+    public class EntryFormModel
+    {
+        [Required]
+        public string userName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailID { get; set; }
+
+        [Required]
+        public string PolicyNo { get; set; }
+
+        [Required]
+        public string DOB { get; set; }
+    }
+}
